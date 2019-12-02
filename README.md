fa43fb81ddc182574c34f198fe4f15ae2b7c7efc
Mmadu Manasseh <mmadumanasseh@gmail.com>
2019-12-02 23:45:44 +0100

    Pipeline: Deploy to Pages

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # The Official Bisoncorps Website 
 
 
+</hr>
+
+[![Build Status](https://travis-ci.com/bisoncorps/bisoncorps.com.svg?branch=master)](https://travis-ci.com/bisoncorps/bisoncorps.com)
+
+</hr>
 Built with Gatsby 
  
 ## Installation
--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -3,6 +3,6 @@ import React from "react"
 import "typeface-roboto"
 import "typeface-roboto-slab"
 // pages for this product
-import Components from "../components/Components"
+// import Components from "../components/Components"
 
-export default () => <Components />
+// export default () => <Components />
